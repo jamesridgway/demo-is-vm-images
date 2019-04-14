e1ca3f268f7becf10cf4a00fe0ed42aa9872bfe0
James Ridgway <myself@james-ridgway.co.uk>
2019-04-14 18:05:16 +0100

    Moving commit version to variable

--- jenkins-master/build.sh
+++ jenkins-master/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-jq '.builders[0].tags.Commit = "'"$(git rev-parse HEAD)"'"' jenkins-master.json > packer-versioned.json
-packer build packer-versioned.json
-rm packer-versioned.json
+packer build -var "commit=$(git rev-parse HEAD)" jenkins-master.json
--- ubuntu-1804/build.sh
+++ ubuntu-1804/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")" || exit 1
-jq '.builders[0].tags.Commit = "'"$(git rev-parse HEAD)"'"' ubuntu.json > packer-versioned.json
-packer build packer-versioned.json
-rm packer-versioned.json
+packer build -var "commit=$(git rev-parse HEAD)" ubuntu.json
